9c60653348e6406d6bcb55c7ae3e93a895933844
Dominik Dünnebacke <dominik@duennebacke.com>
2022-10-19 13:12:00 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,202 @@
 # Sync-NestedAdGroupMember
-Fetches members of AD groups with name suffix -NESTED recursively and syncs them to their -UNNESTED counterpart.
+
+## SYNOPSIS
+Fetches members of AD groups with name suffix `-NESTED` recursively and syncs them to their `-UNNESTED` counterpart.
+
+## SYNTAX
+```powershell
+Sync-NestedAdGroupMember [-SearchBase <String>] [-LegacyPair <Hashtable>] [-Server <String>] [-WhatIf] [-PassThru] [-VERBOSE]
+```
+
+## DESCRIPTION
+The Sync-NestedAdGroupMember.ps1 script syncs members between pairs of groups. A pair consists of two groups with an identical name followed by the suffix `-NESTED` for one group and `-UNNESTED` for the other, where the nested group is the source of truth for the members. You can theoretically create an infinite amount of those pairs in your Active Directory.
+
+During execution the script fetches all AD groups with suffix `-NESTED`, loops thru them and looks for their `-UNNESTED` counterpart. Then all members of the `-NESTED` group are fetched recursively and synced to the `-UNNESTED` group. This means missing members are added and obsolete members are removed. Manual changes to the `-UNNESTED` group are overwritten.
+
+### USE CASE
+There are many applications that do not support nested group membership of users breaking with many access management models. This script serves as a workaround. The idea is that the `-UNNESTED` group is configured within the application and access is managed entirely in the `-NESTED` group.
+
+### REQUIREMENTS
+* PowerShell module [ActiveDirectory](https://learn.microsoft.com/en-us/powershell/module/activedirectory/?view=windowsserver2022-ps)
+* Execution on a domain-joined Windows machine that has an active connection to a domain controller
+* Execution by a user with permission to add/remove members in target AD groups
+
+### INSTALLATION
+* Download the script file:
+    ```powershell
+    Invoke-WebRequest -Uri "https://github.com/dominikduennebacke/Sync-NestedAdGroupMember/raw/main/Sync-NestedAdGroupMember.ps1"
+    ```
+* Set up a scheduled task or a CI/CD job that runs the script every 10 minutes
+
+### SCALING
+After setting up a few group pairs keep an eye on the execution time of the script which should not be larger than the scheduling interval.
+
+## EXAMPLES
+
+### EXAMPLE 1
+Syncs all pairs and provides output.
+```powershell
+Sync-NestedAdGroupMember -VERBOSE
+```
+```
+VERBOSE: Checking dependencies
+VERBOSE: The secure channel between the local computer and the domain is in good condition.
+VERBOSE: Fetching NESTED AD groups
+VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+```
+
+### EXAMPLE 2
+Syncs all pairs and provides output. Two additional group pairs that are outside the naming convention are considered provided by the `LegacyPair` parameter.
+```powershell
+Sync-NestedAdGroupMember -LegacyPair @{"app-dummy-access-NESTED" = "legacyapp1-access"; "app-dummy-access-NESTED" = "legacyapp2-access";} -VERBOSE
+```
+```
+VERBOSE: Checking dependencies
+VERBOSE: The secure channel between the local computer and the domain is in good condition.
+VERBOSE: Fetching NESTED AD groups
+VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) tom.tonkins
+VERBOSE: app-dummy-access-NESTED > legacyapp1-access
+VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) john.doe
+VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) sam.smith
+VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) tom.tonkins
+VERBOSE: app-dummy-access-NESTED > legacyapp2-access
+VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) john.doe
+VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) sam.smith
+VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) tom.tonkins
+```
+
+### EXAMPLE 3
+Provides output of sync changes but does not actually perform them.
+```powershell
+Sync-NestedAdGroupMember -WhatIf:$true
+```
+```
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+```
+
+### EXAMPLE 4
+Provides output of sync changes but does not actually perform them, with additional output.
+```powershell
+Sync-NestedAdGroupMember -WhatIf:$true -VERBOSE
+```
+```
+VERBOSE: Checking dependencies
+VERBOSE: The secure channel between the local computer and the domain is in good condition.
+VERBOSE: Fetching NESTED AD groups
+VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+```
+
+### EXAMPLE 5
+Only consideres the OU `"OU=groups,DC=contoso,DC=com"` looking for group pairs. This can speed up execution.
+```powershell
+Sync-NestedAdGroupMember -SearchBase "OU=groups,DC=contoso,DC=com"
+```
+
+## PARAMETERS
+
+### -SearchBase
+Specifies an Active Directory path to search under.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: 
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -LegacyPair
+Sometimes it can be tedious to replace an access group within an application because of other dependencies or politics. For that additional pairs that are outside the `-NESTED` / `-UNNESTED` naming convention can be provided as a hashtable.
+
+```yaml
+Type: Hashtable
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Server
+Specifies the Active Directory Domain Services instance to connect to.
+
+```yaml
+Type: String
+Parameter Sets: (All)
+Aliases: 
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: False
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -PassThru
+Returns the name of the nested group, name of the unnested group, SamAccountName of the user and modification type (Add or Remove) as PSCustomObject.
+If -PassThru is not specified, this cmdlet does not generate any output.
+
+```yaml
+Type: SwitchParameter
+Parameter Sets: (All)
+Aliases: 
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -VERBOSE
+Provides additional output about the process.
+
+## INPUTS
+
+### Hashtable
+A hashtable is received by the LegacyPair parameter.
+
+## OUTPUTS
+
+### None or PSCustomObject
+Returns the name of the nested group, name of the unnested group, SamAccountName of the user and modification type (Add or Remove) as PSCustomObject if the PassThru parameter is specified.
+By default, this cmdlet does not generate any output.
+
+## LINKS
+* [Blog post on autofocus.duennebacke.com]([./Add-ADPrincipalGroupMembership.md](https://autofocus.duennebacke.com/2022/10/09/nested-active-directory-group-support-for-legacy-apps/))
---
+++ Sync-NestedAdGroupMember.ps1
@@ -0,0 +1,281 @@
+﻿<#
+    .SYNOPSIS
+    Fetches members of AD groups with name suffix -NESTED recursively and syncs them to their -UNNESTED counterpart.
+
+    .DESCRIPTION
+    The Sync-NestedAdGroupMember.ps1 script syncs members between pairs of groups.
+    A pair consists of two groups with an identical name followed by the suffix -NESTED for one group and -UNNESTED for the other,
+    where the nested group is the source of truth for the members. You can theoretically create an infinite amount of those pairs in your Active Directory.
+
+    During execution the script fetches all AD groups with suffix -NESTED, loops thru them and looks for their -UNNESTED counterpart.
+    Then all members of the -NESTED group are fetched recursively and synced to the -UNNESTED group.
+    This means missing members are added and obsolete members are removed.
+    Manual changes to the -UNNESTED group are overwritten.
+
+    .INPUTS
+    Hashtable
+    A hashtable is received by the LegacyPair parameter.
+
+    .OUTPUTS
+    None or PSCustomObject
+    Returns the nested group, unnested group, user and modification type as PSCustomObject if the PassThru parameter is specified. By default, this cmdlet does not generate any output.
+
+    .LINK
+    https://github.com/dominikduennebacke/Sync-NestedAdGroupMember
+
+    .EXAMPLE
+    Syncs all pairs and provides output.
+
+    Sync-NestedAdGroupMember -VERBOSE
+
+    VERBOSE: Checking dependencies
+    VERBOSE: The secure channel between the local computer and the domain is in good condition.
+    VERBOSE: Fetching NESTED AD groups
+    VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+    
+    .EXAMPLE
+    Syncs all pairs and provides output. Two additional group pairs that are outside the naming convention are considered provided by the LegacyPair parameter.
+
+    Sync-NestedAdGroupMember -LegacyPair @{"app-dummy-access-NESTED" = "legacyapp1-access"; "app-dummy-access-NESTED" = "legacyapp2-access";} -VERBOSE
+
+    VERBOSE: Checking dependencies
+    VERBOSE: The secure channel between the local computer and the domain is in good condition.
+    VERBOSE: Fetching NESTED AD groups
+    VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) tom.tonkins
+    VERBOSE: app-dummy-access-NESTED > legacyapp1-access
+    VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) john.doe
+    VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) sam.smith
+    VERBOSE: app-dummy-access-NESTED > legacyapp1-access: (+) tom.tonkins
+    VERBOSE: app-dummy-access-NESTED > legacyapp2-access
+    VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) john.doe
+    VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) sam.smith
+    VERBOSE: app-dummy-access-NESTED > legacyapp2-access: (+) tom.tonkins
+    
+    .EXAMPLE
+    Provides output of sync changes but does not actually perform them.
+
+    Sync-NestedAdGroupMember -WhatIf:$true
+
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+    
+    .EXAMPLE
+    Provides output of sync changes but does not actually perform them, with additional output.
+
+    Sync-NestedAdGroupMember -WhatIf:$true -VERBOSE
+
+    VERBOSE: Checking dependencies
+    VERBOSE: The secure channel between the local computer and the domain is in good condition.
+    VERBOSE: Fetching NESTED AD groups
+    VERBOSE: Syncing group members recursively from NESTED group to UNNESTED group
+    VERBOSE: app-dummy-access-NESTED > app-dummy-access-UNNESTED
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) john.doe
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (+) sam.smith
+    What if: app-dummy-access-NESTED > app-dummy-access-UNNESTED: (-) tom.tonkins
+    
+    .EXAMPLE
+    Only consideres the OU "OU=groups,DC=contoso,DC=com" looking for group pairs. This can speed up execution.
+
+    Sync-NestedAdGroupMember -SearchBase "OU=groups,DC=contoso,DC=com"
+
+    .NOTES
+    - Version: 0.9.0
+    - License: GPL-3.0
+    - Author:   Dominik Dünnebacke
+        - Email:    dominik@duennebacke.com
+        - GitHub:   https://github.com/dominikduennebacke
+        - LinkedIn: https://www.linkedin.com/in/dominikduennebacke/
+#>
+
+
+# -------------------------------------------------------------------------------------------------------------- #
+#region Parameters
+param (
+    # Specifies an Active Directory path to search under
+    [string]$SearchBase,
+
+    # Additional pairs that are outside the `-NESTED` / `-UNNESTED` naming convention
+    [hashtable]$LegacyPair,
+
+    # Specifies the Active Directory Domain Services instance to connect to
+    [string]$Server,
+
+    # Shows what would happen if the cmdlet runs
+    [switch]$WhatIf,
+
+    # Returns the nested group, unnested group, user and modification type as PSCustomObject.
+    [switch]$PassThru,
+
+    # Suffix that is used for NESTED groups
+    [parameter(DontShow)]
+    [string]$NestedSuffix = "-NESTED",
+    
+    # Suffix that is used for UNNESTED groups
+    [parameter(DontShow)]
+    [string]$UnnestedSuffix = "-UNNESTED"
+)
+#endregion Parameters
+
+
+# -------------------------------------------------------------------------------------------------------------- #
+#region Checking dependencies
+Write-Verbose "Checking dependencies"
+
+# Determine if PowerShell module ActiveDirectory is installed
+if (-not (Get-Module -Name ActiveDirectory -ListAvailable)) {
+    throw "PowerShell module 'ActiveDirectory' is not installed"
+}
+
+# Determine domain controller to use for AD cmdlets (if $Server is not set)
+if (-not $Server) {
+    $Server = (Get-ADDomainController -Discover).HostName
+}
+if (-not $Server) {
+    throw "No AD domain controller was found"
+}
+
+
+# Test connection to server
+if (-not (Test-ComputerSecureChannel -Server $Server)) {
+    throw "Connection to $Server failed"
+}
+
+# Determine SearchBase if not set (as cmdlets do not allow an empty SearchBase)
+if (-not $SearchBase) {
+    $SearchBase = (Get-ADDomain -Server $Server).DistinguishedName
+}
+#endregion Checking dependencies
+
+
+# -------------------------------------------------------------------------------------------------------------- #
+#region Fetching NESTED AD groups
+Write-Verbose "Fetching NESTED AD groups"
+
+# Fetching NESTED AD groups by filtering for suffix "-NESTED"
+$Params = @{
+    SearchBase = $SearchBase
+    Filter     = "Name -like '*$($NestedSuffix)'"
+    Server     = $Server
+}
+[array]$NestedGroups = Get-ADGroup @Params
+
+# Fetching NESTED AD groups from LegacyPair parameter
+foreach ($Key in $LegacyPair.Keys) {
+    if ($NestedGroups.Name -notcontains $Key) {
+        $Params = @{
+            SearchBase = $SearchBase
+            Filter     = "Name -eq '$Key'"
+            Server     = $Server
+        }
+        $NestedGroups += Get-ADGroup @Params
+    } 
+}
+
+# Sort groups by name
+$NestedGroups = $NestedGroups | Sort-Object Name
+#endregion Fetching NESTED AD groups
+
+
+# -------------------------------------------------------------------------------------------------------------- #
+#region Syncing group members recursively from NESTED group(s) to UNNESTED group(s)
+Write-Verbose "Syncing group members recursively from NESTED group(s) to UNNESTED group(s)"
+
+foreach ($Group in $NestedGroups) {
+
+    # Building UNNESTED AD group name
+    $UnnestedGroupName = $Group.Name -replace $NestedSuffix, $UnnestedSuffix
+
+    # Finding associated UNNESTED AD group by Get-ADGroup filter query
+    [array]$UnnestedGroups = Get-ADGroup -Filter "name -eq '$UnnestedGroupName'" -Server $Server
+
+    # Finding associated UNNESTED AD group by LegacyPair parameter
+    $UnnestedGroups += foreach ($Value in $LegacyPair.$($Group.Name)) {
+        if ($UnnestedGroups.Name -notcontains $Value) {
+            Get-ADGroup -Identity $Value -Server $Server
+        }
+    }
+
+    # Sort groups by name
+    $UnnestedGroups = $UnnestedGroups | Sort-Object Name
+
+    # Jump to next item in NESTED groups array if no UNNESTED group was found
+    if (-not $UnnestedGroups) {
+        Write-Warning "No associated unnested AD group(s) found for $($Group.Name)"
+        Continue
+    }
+
+    # Fetch members of NESTED group
+    $MembersNested = Get-ADGroupMember -Identity $Group -Recursive -Server $Server | Sort-Object SamAccountName
+
+    # Loop thru UNNESTED group(s)
+    foreach ($UnnestedGroup in $UnnestedGroups) {
+
+        # Output for reference
+        Write-Verbose "$($Group.Name) > $($UnnestedGroup.Name)"
+
+        # Fetch members of UNNESTED group
+        $MembersUnnested = Get-ADGroupMember -Identity $UnnestedGroup.Name -Server $Server | Sort-Object SamAccountName
+
+        # Determine missing and obsolete members of UNNESTED group
+        $MissingMembers = $MembersNested | Where-Object { $MembersUnnested.SID -notcontains $_.SID }
+        $ObsoleteMembers = $MembersUnnested | Where-Object { $MembersNested.SID -notcontains $_.SID }
+
+        # Handle WhatIf
+        if ($WhatIf) {
+
+            # Add missing members to UNNESTED group (WhatIf)
+            foreach ($Member in $MissingMembers) {
+                Write-Host "What if: $($Group.Name) > $($UnnestedGroup.Name): (+) $($Member.SamAccountName)"
+            }
+
+            # Remove missing members from UNNESTED group (WhatIf)
+            foreach ($Member in $ObsoleteMembers) {
+                Write-Host "What if: $($Group.Name) > $($UnnestedGroup.Name): (-) $($Member.SamAccountName)"
+            }    
+        }
+        else {
+
+            # Add missing members to UNNESTED group
+            foreach ($Member in $MissingMembers) {
+                Write-Verbose "$($Group.Name) > $($UnnestedGroup.Name): (+) $($Member.SamAccountName)"
+                Add-ADGroupMember -Identity $UnnestedGroup.Name -Members $Member -Confirm:$false -Server $Server
+
+                # Provide output in case $PassThru is set
+                if ($PassThru) {
+                    [PSCustomObject]@{
+                        NestedGroup   = $Group.Name
+                        UnnestedGroup = $UnnestedGroup.Name
+                        User          = $Member.SamAccountName
+                        Action        = "Add"
+                    }
+                }
+            }
+
+            # Remove missing members from UNNESTED group
+            foreach ($Member in $ObsoleteMembers) {
+                Write-Verbose "$($Group.Name) > $($UnnestedGroup.Name): (-) $($Member.SamAccountName)"
+                Remove-ADGroupMember -Identity $UnnestedGroup.Name -Members $Member -Confirm:$false -Server $Server
+                
+                # Provide output in case $PassThru is set
+                if ($PassThru) {
+                    [PSCustomObject]@{
+                        NestedGroup   = $Group.Name
+                        UnnestedGroup = $UnnestedGroup.Name
+                        User          = $Member.SamAccountName
+                        Action        = "Remove"
+                    }
+                }
+            }
+        }
+    }
+}
+#endregion Syncing group members recursively from NESTED group(s) to UNNESTED group(s)
